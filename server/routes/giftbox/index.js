1523b07d7a4e09751c11e79910550daa1c8581a4
minjunkim <rlaalswns321@naver.com>
2020-09-27 01:41:31 +0900

    - main 라우터 추가  - public 폴더삭제

--- server/routes/giftbox/index.js
+++ server/routes/giftbox/index.js
@@ -6,4 +6,24 @@ router.get('/', function (req, res, next) {
     res.send('This is giftbox');
 });
 
+router.post('/main', function (req, res, next) {
+    try {
+        if (req.body.password.replace(/ /g, "").toLowerCase() == "happybirthday") {
+            res.status(200).json({
+                result: true
+            })
+        } else {
+            res.status(200).json({
+                result: false
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            result: "error"
+        })
+    }
+
+});
+
 module.exports = router;
