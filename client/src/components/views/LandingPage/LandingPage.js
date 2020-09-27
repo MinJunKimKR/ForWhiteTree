705ee7ecad8d428752e4fc1763948433cca03925
minjunkim <rlaalswns321@naver.com>
2020-09-28 00:55:02 +0900

    중첩 라우팅 테스트중

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,25 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import './landing.css'
+import { Route, Link } from "react-router-dom"
 import { Space, Typography, Button } from 'antd';
+import MainLandingPage from './LangingPageMain'
+import SecondPage from './LangdingPage2'
 const { Title, Paragraph, Text } = Typography;
 
 
-function LandingPage() {
+function LandingPage({ match }) {
   useEffect(() => {
     axios.get('/api').then((response) => {
       console.log(response.data);
     });
   }, []);
   return (
-    <Typography style={{ marginTop: '18%', textAlign: 'center' }}>
-      <Title>For the my precious friend White Tree</Title>
-      <Title level={2}>Happy birthday my man<br></br>This page is made only for you</Title>
-
-      <Button style={{ marginTop: '50px', width: '70%', height: '70px', fontSize: '20pt' }} type="primary">
-        로그인 하러가기
-      </Button>
-    </Typography>
+    <>
+      <Route exact path={match.path} component={MainLandingPage} />
+      <Route exact path={`${match.path}/second`} component={SecondPage} />
+    </>
   );
 }
 
---
+++ client/src/components/views/LandingPage/LangingPageMain.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import './landing.css'
+import { Route, Link } from "react-router-dom"
+import { Space, Typography, Button } from 'antd';
+const { Title, Paragraph, Text } = Typography;
+
+
+function LandingPage({ match }) {
+    useEffect(() => {
+        axios.get('/api').then((response) => {
+            console.log(response.data);
+        });
+    }, []);
+    return (
+        <Typography style={{ marginTop: '18%', textAlign: 'center' }}>
+            <Title>For the my precious friend White Tree</Title>
+            <Title level={2}>Happy birthday my man<br></br>This page is made only for you</Title>
+            <Link to={`${match.url}/second`}>
+                <Button style={{ marginTop: '50px', width: '70%', height: '70px', fontSize: '20pt' }} type="primary">
+                    로그인 하러가기
+      </Button>
+            </Link>
+        </Typography>
+    );
+}
+
+export default LandingPage;
